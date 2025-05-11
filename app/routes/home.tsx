d50669dcfa810d15c32c709215ac20709ef803d8
ogatakatsuya <katsuya151225@icloud.com>
2025-05-11 20:45:40 +0900

    feat: ログアウトボタンをcomponent化

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,11 +4,12 @@ import {
 	redirect,
 } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
-import { Form } from "@remix-run/react";
+
+import { fetchGitHubApi } from "~/utils/github.server";
+import SignOutButton from "~/components/SignOutButton";
 import { serializeCookieHeader } from "@supabase/ssr";
 
 import { signOut } from "~/utils/auth.server";
-import { fetchGitHubApi } from "~/utils/github.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
 
@@ -22,20 +23,20 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
-	const { data, headers } = await signOut(request, context);
+    const { data, headers } = await signOut(request, context);
 
-	headers.append(
-		"Set-Cookie",
-		serializeCookieHeader("58hack-github-token", "", {
-			httpOnly: true,
-			secure: true,
-			path: "/",
-			sameSite: "lax",
-			expires: new Date(0),
-		}),
-	);
+    headers.append(
+        "Set-Cookie",
+        serializeCookieHeader("58hack-github-token", "", {
+            httpOnly: true,
+            secure: true,
+            path: "/",
+            sameSite: "lax",
+            expires: new Date(0),
+        }),
+    );
 
-	return redirect(data.url, { headers });
+    return redirect(data.url, { headers });
 };
 
 export default function AuthCode() {
@@ -63,26 +64,7 @@ export default function AuthCode() {
 					</div>
 				)}
 
-				<Form method="post">
-					<button
-						type="submit"
-						className="flex w-full items-center justify-center rounded-md bg-red-600 px-4 py-3 text-white transition-colors hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="mr-2 h-5 w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								fillRule="evenodd"
-								d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-								clipRule="evenodd"
-							/>
-						</svg>
-						<span className="text-base font-medium">ログアウト</span>
-					</button>
-				</Form>
+				<SignOutButton />
 			</div>
 		</div>
 	);
