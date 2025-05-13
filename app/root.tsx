5779b8cdb2f410ce0aabd81c341709ae842f03b4
zatunohito <zatunohito@gmail.com>
2025-05-13 20:15:34 +0900

    5-randomPlace

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from "@remix-run/react";
+import { CreateRoomPage } from "./routes/createTo";
 
 import "./tailwind.css";
 
@@ -32,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
+				{true && <CreateRoomPage />}
 				{children}
 				<ScrollRestoration />
 				<Scripts />
